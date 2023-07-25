3e199418149bc124c96546ec88b35b8b786ed7fd
ElvisGalvez <galvez.elvis@gmail.com>
2023-07-25 17:41:47 +0200

    Home done

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,6 +1,14 @@
+import React from 'react';
+import Banner from '../../components/Banner';
+import HousingCardList from '../../components/HousingCardList';
 
 function Home() {
-
+  return (
+    <div>
+      <Banner />
+      <HousingCardList />
+    </div>
+  );
 }
 
 export default Home;

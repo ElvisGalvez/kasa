b3bd2580e2f2efa01c3bd20e2c5676e2a7656885
ElvisGalvez <galvez.elvis@gmail.com>
2023-07-26 15:27:31 +0200

    Housing detail 10%

--- src/pages/HousingDetail/index.jsx
+++ src/pages/HousingDetail/index.jsx
@@ -1,12 +1,24 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
+import housingData from '../../data/logements.json';
+import Carousel from '../../components/Carousel';  
 
 const HousingDetail = () => {
+  const { id } = useParams();
+  const housing = housingData.find(housing => housing.id === id);
+
+  // handle case where housing with id does not exist
+  if (!housing) {
+    return <p>Housing not found</p>;
+  }
+
   return (
     <div>
-      <h1>Housing Detail Page</h1>
-      <p>This is a placeholder for the housing detail page.</p>
+      
+      <Carousel images={housing.pictures} />
+
     </div>
   );
-}
+};
 
 export default HousingDetail;

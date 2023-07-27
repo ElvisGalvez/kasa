67376c2c4d7984d1f05aacd2c1e97b82b42c7238
ElvisGalvez <galvez.elvis@gmail.com>
2023-07-27 19:39:57 +0200

    desktop done

--- src/components/Accordion/index.jsx
+++ src/components/Accordion/index.jsx
@@ -1,17 +1,22 @@
 import React, { useState } from 'react';
 import './Accordion.css';
 
-const Accordion = ({ title, content }) => {
+const Accordion = ({ title, content, customClass }) => {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <div className="accordion-wrapper">
-      <button className="accordion-button" onClick={() => setIsOpen(!isOpen)}>
+    <div className={`accordion-wrapper ${customClass}`}>
+      <button 
+        className="accordion-button" 
+        onClick={() => setIsOpen(!isOpen)}
+        aria-expanded={isOpen}  
+      >
         {title}
         <span className="accordion-icon">
-  {isOpen ? <i className="fas fa-chevron-up"></i> : <i className="fas fa-chevron-down"></i>}
-</span>
+          {isOpen ? <i className="fas fa-chevron-up"></i> : <i className="fas fa-chevron-down"></i>}
+        </span>
       </button>
+
       {isOpen && <div className="accordion-content">{content}</div>}
     </div>
   );

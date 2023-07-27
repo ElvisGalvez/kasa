c8f51aa635aca61b40aa9bd60d3f7335277affa1
ElvisGalvez <galvez.elvis@gmail.com>
2023-07-27 19:40:11 +0200

    desktop done

--- src/components/Carousel/index.jsx
+++ src/components/Carousel/index.jsx
@@ -2,32 +2,36 @@ import React, { useState } from "react";
 import "./Carousel.css";
 
 const Carousel = ({ images }) => {
-    const [currentIndex, setCurrentIndex] = useState(0);  
-  
-    const nextImage = () => {
-      setCurrentIndex((currentIndex + 1) % images.length);  
-    };
-  
-    const prevImage = () => {
-      setCurrentIndex((currentIndex - 1 + images.length) % images.length);  
-    };
-  
-    return (
-      <div className="carousel-container">
-        {images.length > 1 && (
-          <button onClick={prevImage} className="carousel-button prev" aria-label="Previous slide">
-            <i className="fas fa-chevron-left"></i>
-          </button>
-        )}
-        <img src={images[currentIndex]} alt={`Slide ${currentIndex + 1}`} className="carousel-image" />
-        {images.length > 1 && (
-          <button onClick={nextImage} className="carousel-button next" aria-label="Next slide">
-            <i className="fas fa-chevron-right"></i>
-          </button>
-        )}
-      </div>
-    );
+  const [currentIndex, setCurrentIndex] = useState(0);  
+
+  const nextImage = () => {
+    setCurrentIndex((currentIndex + 1) % images.length);  
   };
-  
-  export default Carousel;
-  
+
+  const prevImage = () => {
+    setCurrentIndex((currentIndex - 1 + images.length) % images.length);  
+  };
+
+  return (
+    <div className="carousel-container">
+      {images.length > 1 && (
+        <button onClick={prevImage} className="carousel-button prev" aria-label="Previous slide">
+          <i className="fas fa-chevron-left"></i>
+        </button>
+      )}
+      <img src={images[currentIndex]} alt={`Slide ${currentIndex + 1}`} className="carousel-image" />
+      {images.length > 1 && (
+        <button onClick={nextImage} className="carousel-button next" aria-label="Next slide">
+          <i className="fas fa-chevron-right"></i>
+        </button>
+      )}
+      {images.length > 1 && (
+        <div className="carousel-position">
+          <p>{currentIndex + 1}/{images.length}</p>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Carousel;
--- src/pages/HousingDetail/index.jsx
+++ src/pages/HousingDetail/index.jsx
@@ -1,23 +1,76 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import housingData from '../../data/logements.json';
-import Carousel from '../../components/Carousel';  
+import Carousel from '../../components/Carousel';
+import Accordion from '../../components/Accordion';
+import './HousingDetail.css';  
 
 const HousingDetail = () => {
   const { id } = useParams();
   const housing = housingData.find(housing => housing.id === id);
 
-  // handle case where housing with id does not exist
+  // Gérer le cas où le logement avec l'id n'existe pas
   if (!housing) {
-    return <p>Housing not found</p>;
+    return <p>Logement non trouvé</p>;
+  }
+
+  const hostName = housing.host.name.split(' ');
+
+  const rating = parseInt(housing.rating);
+  const stars = [];
+  for (let i = 1; i <= 5; i++) {
+    stars.push(i <= rating ? 'fa-star full-star' : 'fa-star empty-star');
   }
 
   return (
     <div>
-      
       <Carousel images={housing.pictures} />
 
-    </div>
+      <div className="title-host-container">
+        <div>
+          <h1 className="housing-title">{housing.title}</h1>
+          <p className="housing-location">{housing.location}</p>
+          
+          <div className="tags-container">
+            {housing.tags.map((tag, index) => (
+              <p key={index} className="tag">{tag}</p>
+            ))}
+          </div>
+        </div>
+
+        <div className="host-container">
+          <div className="host">
+            <div className="host-name">
+              <p>{hostName[0]}</p>
+              <p>{hostName[1]}</p>
+            </div>
+            <img src={housing.host.picture} alt={`Host ${housing.host.name}`} className="host-picture" />
+          </div>
+          <div className="rating">
+            {stars.map((star, index) => (
+              <i key={index} className={`fas ${star}`}></i>
+            ))}
+          </div>
+        </div>
+      </div>
+
+      <div className="accordion-container">
+    <Accordion 
+        title="Description"
+        content={housing.description}
+        customClass="accordion--housePage"
+    />
+    <Accordion 
+        title="Équipements"
+        content={housing.equipments.map((e, index) => <p key={`${e}-${index}`}>{e}</p>)}
+        customClass="accordion--housePage"
+    />
+</div>
+
+
+
+        </div>
+      
   );
 };
 
